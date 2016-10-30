0ec2dcf341e9a945d4834a5ad485cc3424fb05d3
GoodDinosaur <wilson.sun@hotmail.ca>
2016-10-30 01:15:14 -0400

    Update PartitionEqualSubsetSum.cpp

--- DP/PartitionEqualSubsetSum.cpp
+++ DP/PartitionEqualSubsetSum.cpp
@@ -1,27 +1,23 @@
 bool canPartition(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    
     int len = (int)nums.size();
-    int firstPVal = 0;
-    int secondPVal = 0;
+    int sum = 0;
     
     for (int i = 0; i < len; ++i) {
-        if (i < len/2) {
-            firstPVal += nums[i];
-        } else {
-            secondPVal += nums[i];
+        sum += nums[i];
+    }
+    
+    if (sum%2 != 0)
+        return false;
+    
+    int target = sum/2;
+    vector<bool> dp(target+1, 0);
+    dp[0] = true;
+    
+    for (int i = 0; i < len; ++i) {
+        for (int j = target; j >= nums[i]; --j) {
+            dp[j] = dp[j] || dp[j-nums[i]];
         }
     }
     
-    if (firstPVal == secondPVal)
-        return true;
-    
-    for (int i = len/2; i < len-1; ++i) {
-        firstPVal += nums[i];
-        secondPVal -= nums[i];
-        if (firstPVal == secondPVal)
-            return true;
-    }
-    
-    return false;
+    return dp.back();
 }
