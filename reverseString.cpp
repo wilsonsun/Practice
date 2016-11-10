d309b3130706b3f895ae1705732879a2627a104c
GoodDinosaur <wilson.sun@hotmail.ca>
2016-11-10 00:42:12 -0500

    Update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,23 +1,29 @@
 void reverseWords(string &s) {
     vector<string> stack;
     int slen = (int)s.size();
-    if (s == "")
-        return;
     
     string temp = "";
     for (int i = 0; i < slen; ++i) {
         if (s[i] == ' ') {
+            if (temp == "")
+                continue;
             stack.push_back(temp);
             temp = "";
         } else {
             temp += s[i];
         }
     }
-    
-    stack.push_back(temp);
+    if (temp != "")
+        stack.push_back(temp);
     
     s = "";
+    if (!stack.empty()) {
+        s += stack.back();
+        stack.pop_back();
+    }
+    
     while (!stack.empty()) {
+        s += ' ';
         s += stack.back();
         stack.pop_back();
     }
